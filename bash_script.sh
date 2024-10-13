3e4d4707c2b5e36811cf33e71526e300a091f759
Manoj Kumar <90147485+manojmahan@users.noreply.github.com>
2024-10-14 01:59:46 +0530

    Update bash_script.sh

--- bash_script.sh
+++ bash_script.sh
@@ -2,13 +2,15 @@
 
 BED_FILE="data/mapped.bed.gz"
 
+# read the bed file and used the linear transformation in order to rescale the mid-value in (-500,500)
+# and convert the transformed value to integer
 zcat "$BED_FILE" | awk '{
     # Perform the transformation
     transformed_value3 = (1000 / ($4 -$3)) * (($9+$10)/2 -$3) - 500;
     print transformed_value3,$12;
 }'| awk '{print ($1 < 0) ? int($1) - 1 : int($1), $2} ' | tr ' ' '\t' > new_output.tsv
        
-#sort |uniq -c | awk '{print $1, $2, $3}' | tr ' ' '\t' > output.tsv
 
+# python script to calculate the frequency and plot the vplot using matplotlib
 
 python3 main.py new_output.tsv
